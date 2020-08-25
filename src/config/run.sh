f2f1f71c51ca057e274e320e0876ddbdbd364a14
lcsvncs429@gmail.com <lcsvncs429@gmail.com>
2020-08-25 12:35:21 -0300

    refactoring

--- src/config/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /app/
-/usr/sbin/crond -f -l 8 &
-npm start 
--- src/config/theme.js
+++
@@ -1,8 +0,0 @@
-import { red } from "@material-ui/core/colors";
-
-export const {
-  PRIMARY_COLOR = "#556cd6",
-  SECUNDARY_COLOR = "#19857b",
-  ERROR_COLOR = red.A400,
-  BACKGROUND_COLOR = "#fff"
-} = process.env;
